51d6e12970976d4c2b4bc1edd7aef2286643880d
superhsu <31141265+superhsu@users.noreply.github.com>
2018-02-13 20:13:20 -0800

    done with challenge 2

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,45 @@
+//this is the client side 
+//use ajax request to talk to the server 
+//send JSON data to server
+
+var addNode = function(results) {
+    var split = results.split('\n');
+    for (var i = 0; i < split.length; i++) {
+        $('.attach').append(`<div> ${split[i]} </div>`);
+    }
+  }
+
+//create an object to store methods.
+
+var app = {}; 
+
+//app.send is sending a string or messageObj to server.js
+app.send = function (message) {
+  $.ajax({
+    type: 'POST',
+    url: '/', //this is the url where client side ajax will post request to which is my local machine right now or can use '/' cause same domain root 
+    data: message, //always needs to send json data to servers
+    contentType: 'application/json', //needs to tell server what type of data we are sending 
+    success: function (result) {
+        console.log('success!')
+        //append the successful data result you receive back to the dom
+        addNode(result);
+    },
+    error: function () {
+        console.log('err')  
+    }
+  })
+}
+
+//jquery input value capture 
+//jquery on submit listener
+// the $inputText needs to be in the document click function because when you click it needs to capture the value. previously I used it outside the document function and it never captures the value because it only has value when the page loaded
+  $(document).on('click', '#subtn', function(){
+    let $inputText = $('#hi').val(); 
+    console.log('clicked!');
+    app.send($inputText); 
+})
+
+
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,82 @@
+var express = require('express');
+var bodyParser = require('body-parser'); //middleware should always start at the top 
+var app = express(); 
+
+app.use(bodyParser.json());
+//.use is a method that adds the middleware that we specifiy 
+// app.get('/', (req, res) => res.send('hello')) // just used for test connection 
+app.use(express.static('client')); //serves my index.html to whoever made the get request. looks throught the client folder looking for any statis assets in this case index.html 
 
 
+
+//you're using app.post for the post request type from ajax  
+app.post('/', (req,res) => { 
+console.log(combo(req.body));
+  res.send(combo(req.body));
+});
+
+var header = function(obj) {
+    var temp = []; 
+    let csv = '';
+    var keys = Object.keys(obj); 
+    keys.pop(); 
+    var joinedKeyRow = keys.join(', ')
+    joinedKeyRow += '\n';
+    return joinedKeyRow;
+}
+
+var convertFile = function(obj) {
+  var temp = []; 
+  let csv = '';
+  var keys = Object.keys(obj); 
+//   keys.pop(); 
+//   var joinedKeyRow = keys.join(', ')
+
+for(var i = 0; i < keys.length - 1; i++) {
+    if (i !== keys.length - 2) {
+        csv += obj[keys[i]]; 
+        csv += ','; 
+    } else {
+        csv += obj[keys[i]];
+        csv += '\n';
+        if (obj['children'].length !== 0) {
+            for (var j = 0; j < obj['children'].length; j++) {
+                csv += convertFile(obj['children'][j]);
+            }
+        }
+    }
+  }
+//   temp.push(joinedKeyRow); 
+//   temp.push(csv);
+//   return temp;
+  return csv; 
+}
+
+var combo = function(obj) {
+    var temp = ''; 
+    temp += header(obj);
+    temp += convertFile(obj);
+    return temp;
+}
+  //use for loop to get all key values 
+  //if children.length === 0 return 
+  //if not use recursion for loop 
+//   var anotherTemp = []; 
+//   for (var key in obj) {
+//       if (key !== 'children') {
+//         anotherTemp.push(obj[key]);
+//       }
+//       if (key === 'children') {
+//           if (obj[key].length === 0) {
+              
+//           }
+//       }
+//   } cant use this way will be out of order. 
+
+
+
+
+//in app.post the req.body will contain the message/data that the ajax posted to the server 
+//then you use res.send to send back the data 
+
+app.listen(3000, () => console.log('listening!'));
